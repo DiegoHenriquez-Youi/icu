eda3696951c54b44e2aec983e228675bc528cc91
Diego Henriquez <DiegoHenriquez-Youi@youi.tv>
2019-04-25 13:11:53 -0400

    test

--- source/data/cmake/host/cmake/Hunter/config.cmake
+++ source/data/cmake/host/cmake/Hunter/config.cmake
@@ -1,2 +1,2 @@
-hunter_config(ICU VERSION 55.1-p1)
-#hunter_config(ICU GIT_SUBMODULE "thirdparty/icu" CMAKE_ARGS BUILD_FOR_PS4=ON)
+#hunter_config(ICU VERSION 55.1-p1)
+hunter_config(ICU GIT_SUBMODULE "thirdparty/icu")

732fc2b1b06daf4d062415da10d4632a45227187
Diego Henriquez <DiegoHenriquez-Youi@youi.tv>
2019-04-25 15:56:11 -0400

    test

--- source/data/cmake/host/cmake/Hunter/config.cmake
+++ source/data/cmake/host/cmake/Hunter/config.cmake
@@ -1,2 +1,2 @@
-#hunter_config(ICU VERSION 55.1-p1)
-hunter_config(ICU GIT_SUBMODULE "thirdparty/icu")
+hunter_config(ICU VERSION 55.1-p1)
+#hunter_config(ICU GIT_SUBMODULE "thirdparty/icu") #CMAKE_ARGS CYGWINMSVC=ON)
